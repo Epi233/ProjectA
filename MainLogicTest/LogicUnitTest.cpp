ca05791c0406f8f1265d3f8f635b0decb0cd173f
yinchen0 <yinchen0621@foxmail.com>
2019-12-08 14:14:00 +0800

    test

--- MainLogicTest/LogicUnitTest.cpp
+++ MainLogicTest/LogicUnitTest.cpp
@@ -3,6 +3,7 @@
 #include "Components/LogicUnit.hpp"
 #include "Components/ComponentInterface.hpp"
 #include "lua.hpp"
+#include "iostream"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -14,12 +15,13 @@ namespace MainLogicTest
 		TEST_METHOD(logicUnitTest1)
 		{
 			// test LogicUnit<EMPTY>
-			ProjectA::LogicUnit<ProjectA::EMPTY> pureLogicTest(string("pureLogicTest.lua"));
+			//ProjectA::LogicUnit<ProjectA::EMPTY> pureLogicTest("../MainLogicTest/src/ModuleTestResource/pureLogicTest.lua");
+			ProjectA::LogicUnit<ProjectA::EMPTY> pureLogicTest("../ModuleTestResource/pureLogicTest.lua");
 
 			// make fake ports to send/load data to/from the real ports
-			ProjectA::Port input1(ProjectA::WidthSpec(32));
-			ProjectA::Port input2(ProjectA::WidthSpec(32));
-			ProjectA::Port output1(ProjectA::WidthSpec(32));
+			ProjectA::Port input1(ProjectA::WidthSpec{ 32 });
+			ProjectA::Port input2(ProjectA::WidthSpec{ 32 });
+			ProjectA::Port output1(ProjectA::WidthSpec{ 32 });
 
 			// define input ports and output ports
 			vector<ProjectA::WidthSpec> inPorts{ ProjectA::WidthSpec{32}, ProjectA::WidthSpec{32} };
@@ -37,23 +39,33 @@ namespace MainLogicTest
 			pureLogicTest.getOutPortPtr(uint64_t(0))->setTargetPort(&output1);
 
 			// assign data to the fake input ports
-			vector<uint64_t> widthSpec{32};
+			vector<uint64_t> widthSpec = {32};
 			ProjectA::Data din1(widthSpec);
 			ProjectA::Data din2(widthSpec);
 			ProjectA::Data dout1(widthSpec);
 
-			din1.setValue(vector<int64_t>{458});
-			din2.setValue(vector<int64_t>{129});
+			din1.setValue(vector<int64_t>{5});
+			din2.setValue(vector<int64_t>{7});
+
+			input1.setData(din1);
+			input2.setData(din2);
+
+			Logger::WriteMessage((input1.getData()).getDataString<int64_t>().c_str());
 
 			input1.run();
 			input2.run();
 
+			Logger::WriteMessage((input1.getData()).getDataString<int64_t>().c_str());
+			Logger::WriteMessage((input2.getData()).getDataString<int64_t>().c_str());
+
 			pureLogicTest.run();  // .lua file name must consist with the luaAddr
 
-			Logger::WriteMessage(string("123").c_str());
+			Logger::WriteMessage((output1.getData()).getDataString<int64_t>().c_str());
+
+			//Logger::WriteMessage(string("123").c_str());
 			//Logger::WriteMessage((output1.getData()).getDataCells<string>()[0].c_str());
-			//Assert::AreEqual(int64_t(587), (output1.getData()).getDataCells<int64_t>()[0]);  // vertify _dataCells[0] 
-			Assert::AreEqual(11, 11);
+			Assert::AreEqual(int64_t(12), (output1.getData()).getDataCells<int64_t>()[0]);  // vertify _dataCells[0] 
+			//Assert::AreEqual(11, 11);
 		}
 	};
 }
